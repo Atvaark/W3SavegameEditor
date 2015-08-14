61f959bdb694334ab33716a24599724ced7c4b5c
Atvaark <Atvaark@users.noreply.github.com>
2015-08-14 08:47:29 +0200

    Added a few more root variable types

---
+++ W3SavegameEditor.Core/Savegame/Values/CWitcherGameResource.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace W3SavegameEditor.Core.Savegame.Values
+{
+    [CName("CWitcherGameResource")]
+    public class CWitcherGameResource
+    {
+        [CName("path")]
+        public string Path { get; set; }
+    }
+}
---
+++ W3SavegameEditor.Core/Savegame/Values/GameProp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace W3SavegameEditor.Core.Savegame.Values
+{
+    [CName("gameProp")]
+    public class GameProp
+    {
+        [CName("EngineTime")]
+        public double EngineTime { get; set; }
+
+        [CName("nP")]
+        public uint Np { get; set; }
+
+        [CName("zoneName")]
+        public W3Enum ZoneName { get; set; }
+
+        [CName("recentDialogOrCutsceneEndGameTime")]
+        public GameTime RecentDialogOrCutsceneEndGameTime { get; set; }
+
+        public object Unknown1 { get; set; }
+
+        [CName("gameplayFactsForRemoval")]
+        public SGameplayFactRemoval[] GameplayFactsForRemoval { get; set; }
+
+
+        [CName("gameplayFacts")]
+        public SGameplayFact[] GameplayFacts { get; set; }
+
+        [CName("tutorialManagerHandle")]
+        public EntityHandle TutorialManagerHandle { get; set; }
+
+        [CName("diffChangePostponed")]
+        public W3Enum DiffChangePostponed { get; set; }
+
+        [CName("dynamicallySpawnedBoats")]
+        public EntityHandle[] DynamicallySpawnedBoats { get; set; }
+
+        [CName("dynamicallySpawnedBoats")]
+        public EntityHandle[] DynamicallySpawnedBoatsToDestroy { get; set; }
+
+        [CName("envMgr")]
+        public Handle<W3EnvironmentManager> EnvMgr { get; set; }
+
+        public object Unknown2 { get; set; }
+    }
+}
